58bc7270cfb033693ce11fea1949b17146a601c1
hadishaikh <74302409+hadishaikh@users.noreply.github.com>
2020-11-13 14:39:13 +0530

    update highscore

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ var score=0;
 
 var gameOver, restart;
 
-localStorage["HighestScore"];
+localStorage["HighestScore"] = 0;
 
 function preload(){
   trex_running =   loadAnimation("trex1.png","trex3.png","trex4.png");
